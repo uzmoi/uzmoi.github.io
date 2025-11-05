ee0e5697bdad2aa3a8362719dd2920a8c0ca63d8
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-11-05 21:20:46 +0900

    avatar画像を最適化する

--- astro.config.ts
+++ astro.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://uzmoi.github.io/",
+  image: {
+    domains: ["avatars.githubusercontent.com"],
+  },
 });
